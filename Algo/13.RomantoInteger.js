4e8c08d1463b4f742407785874fae60045e573e1
Calvin Park <dev.calvinpark@gmail.com>
2022-11-29 13:14:43 +0900

    feat: 13 solved

--- Algo/13.RomantoInteger.js
+++ Algo/13.RomantoInteger.js
@@ -11,5 +11,32 @@
 
 // 예를 들어 2는 II.
 // 12는 XII이다. X + II
-// 12는 XXVII이다. X + X + V + I + I이다.
+// 27는 XXVII이다. X + X + V + I + I이다.
+// 1994년은 MCMXCIV이다. M + C + M
 
+let stringToInt = (s) => {
+    const sym = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+   let result = 0;
+   for (let i=0;i<s.length;i++){
+    const current = sym[s[i]];
+    const next = sym[s[i+1]];
+    if(current < next) {
+        result += next - current;
+         i++;
+        }
+    else {
+        result += current
+    }
+   }
+    return result;    
+}
+console.log(stringToInt("MCMXCIV"));
